b30c9b8e925acbb171c23f34f3de611c8f03033e
yu-jin-song <yujinsong.tech@gmail.com>
2021-06-18 19:11:38 +0900

    20210618 3단계 10171 고양이

---
+++ boj/step1/_10171.java
@@ -0,0 +1,11 @@
+
+public class _10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+
+}
