a9bd0e67d22a43dab0430f58f1978fd116bd5b81
yu-jin-song <yujinsong.tech@gmail.com>
2021-06-20 18:35:44 +0900

    20210620 7단계 11021 A+B - 7

---
+++ boj/step3/_11021.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _11021 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int sum = 0;
+			
+			while(st.hasMoreTokens())
+				sum += Integer.parseInt(st.nextToken());
+				
+			bw.write("Case #" + i + ": " + sum + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
