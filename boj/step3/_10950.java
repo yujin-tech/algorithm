15f6c39284b85c9811603b7d8fdbeadbd17533f8
yu-jin-song <yujinsong.tech@gmail.com>
2021-06-20 17:36:08 +0900

    20210620 2단계 10950 A+B - 3

---
+++ boj/step3/_10950.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class _10950 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = Integer.parseInt(sc.nextLine());
+		
+		for(int i=0; i<t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+		
+	}
+}
